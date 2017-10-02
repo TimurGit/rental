78bcdfc29ceff7b7ab3a9c9b9768e5f8c6320a00
timurmustafin <mustafintim@gmail.com>
2017-10-02 20:21:34 +0500

    selected value

--- rental/database/seeds/TransportLocationsTableSeeder.php
+++ rental/database/seeds/TransportLocationsTableSeeder.php
@@ -13,21 +13,30 @@ class TransportLocationsTableSeeder extends Seeder
     {
         $transports = \App\Models\Transport::all();
         $facker = Faker\Factory::create();
+        $arrayPoints = [];
+//        Всего 5 тысяч точек
+        for ($i=1;$i<=5000; $i++)
+        {
+            $arrayPoints[$i]['lat'] = $facker->randomFloat(4,-90,90);
+            $arrayPoints[$i]['lon'] = $facker->randomFloat(4,-180,180);
+        }
+
         foreach ($transports as $transport)
         {
             $countPoints = $facker->numberBetween(20,100);
-            $lat = $facker->randomFloat(14,-90,90);
-            $lon = $facker->randomFloat(14,-180,180);
+//            $lat = $facker->randomFloat(14,-90,90);
+//            $lon = $facker->randomFloat(14,-180,180);
 
             for ($i = 1; $i <= $countPoints; $i++) {
 
+                $randomArrayPoints = $facker->numberBetween(1,5000);
                 \App\Models\TransportLocation::create([
                     'transport_id'=>$transport->id,
-                    'lat'=>$lat,
-                    'lon'=>$lon
+                    'lat'=>$arrayPoints[$randomArrayPoints]['lat'],
+                    'lon'=>$arrayPoints[$randomArrayPoints]['lon'],
                 ]);
-                $lat += $facker->randomFloat(14,-1,1);
-                $lon += $facker->randomFloat(14,-1,1);
+//                $lat += $facker->randomFloat(4,-1,1);
+//                $lon += $facker->randomFloat(4,-1,1);
 
             }
         }
